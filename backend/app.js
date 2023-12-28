8917c352c8a92be5e1bb8ef34a11d0a69e626e85
rajchatterz <iamraj.space@gmail.com>
2023-12-28 13:13:43 +0530

    done with the api

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.get('/eg', (req, res) => {
     res.send("hehheehh")
 })
@@ -29,12 +30,36 @@ app.post('/register', async(req, res) => {
         res.status(500).json(error)
     }
 })
-// app.get('/admin', async(req, res) => {
-//     const admin = await Product.find({})
-//     res.status(200).json(admin)
-// })
+app.get('/admin', async(req, res) => {
+    try {
+        const resposne = await Product.find({})
+        res.status(200).json(resposne)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+app.get('/register', async (req, res) => {
+    try {
+        const response = await AuthSchema.find({})
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+app.get('/register/:email', async (req, res) => {
+    try {
+        const email = req.params.email;
+        const user = await AuthSchema.findOne({ email }); // Changed to AuthSchema
+        if (!user) {
+            return res.status(404).json({ message: "No valid user" });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 
 app.listen(3001)
 mongoose.set('strictQuery', false)
-mongoose.connect('mongodb+srv://rajchatterz:IWlj6fLZ330S2y5H@edtech.df872ze.mongodb.net/?retryWrites=true&w=majority').then((con)=>console.log(`server connected ${con.Connection.EventEmitterAsyncResource}`))
+mongoose.connect('mongodb+srv://rajchatterz:IWlj6fLZ330S2y5H@edtech.df872ze.mongodb.net/?retryWrites=true&w=majority').then((con)=>console.log(`server connected ${con.Connection.name}`)).catch((err)=>console.log(err))
