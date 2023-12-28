3f05efeec9366790b7a2759db54d3b9433a9f8ee
rajchatterz <iamraj.space@gmail.com>
2023-12-28 13:38:25 +0530

    start workingg on the navigation

--- front/App.tsx
+++ front/App.tsx
@@ -1,21 +1,10 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React from 'react';
+
 const App = () => {
-  const [data, setData] = useState([]);
-  const producrData = async () => {
-    const fetchData = await axios.get('http://192.168.215.43:3001/admin');
-    const response = fetchData.data;
-    setData(response);
-  };
-  useEffect(() => {
-    producrData();
-  }, [data]);
   return (
     <View>
-      {data.map((e, index) => (
-        <Text key={index}>{e.name}</Text>
-      ))}
+      <Text>App</Text>
     </View>
   );
 };
