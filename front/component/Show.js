6dbc983d410ceb3592e2dbf7eb72dfbb3de5b9d9
rajchatterz <iamraj.space@gmail.com>
2023-12-31 23:54:30 +0530

    working with action

---
+++ front/component/Show.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Show = () => {
+  return (
+    <View>
+      <Text style={{fontSize:30}}>0</Text>
+    </View>
+  )
+}
+
+export default Show
+
+const styles = StyleSheet.create({})
